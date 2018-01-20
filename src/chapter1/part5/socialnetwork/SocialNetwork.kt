cf76c15960d2c7020c3e9637a3ed0cb3a2d6d30e
Zemlyakov Stas <redtom@yandex.ru>
2018-01-20 21:55:37 +0300

    2x compression implemented. 100000000 members are connected in ~12sec.

--- src/chapter1/part5/socialnetwork/SocialNetwork.kt
+++ src/chapter1/part5/socialnetwork/SocialNetwork.kt
@@ -9,11 +9,20 @@ class SocialNetwork(private val membersAmount: Int) {
 
     fun makeFriendships() {
         val timer = Stopwatch()
-        for (i in 0 until membersAmount / 2) {
+
+        for (i in 0 until membersAmount / 4) {
+            /* Block #1 */
             members.union(i, i + 1) // from one side
 
-            val fromOtherSide = membersAmount - 1 - i
-            members.union(fromOtherSide, fromOtherSide - 1)
+            val b1fromOtherSide = membersAmount / 2 - 1 - i
+            members.union(b1fromOtherSide, b1fromOtherSide - 1)
+
+            /* Block #2 */
+            members.union(membersAmount / 2 + i, membersAmount / 2 + i + 1)
+            val b2fromOtherSide = membersAmount / 1 - 1 - i
+            members.union(b2fromOtherSide, b2fromOtherSide - 1)
+
+            members.union(b1fromOtherSide, b2fromOtherSide)
 
             timestamps.add(timer.elapsedTime())
         }
@@ -25,7 +34,7 @@ class SocialNetwork(private val membersAmount: Int) {
 }
 
 fun main(args: Array<String>) {
-    val sn = SocialNetwork(10000000)
+    val sn = SocialNetwork(100000000)
     sn.makeFriendships()
     println(sn.timestamps.last())
     println("All are friends: ${sn.allAreFriends()}")
