45df362509d9d430d4013fe37d54052a2c3daeda
Zemlyakov Stas <redtom@yandex.ru>
2018-01-26 05:54:18 +0300

    Percolation initial commit.

---
+++ src/chapter1/part5/percolation/Percolation.java
@@ -0,0 +1,24 @@
+package chapter1.part5.percolation;
+
+public class Percolation {
+
+    public Percolation(int n) {
+
+    }
+
+    public boolean isFull(int row, int col) {
+        return false;
+    }
+
+    public boolean isOpen(int row, int col) {
+        return false;
+    }
+
+    public boolean percolates() {
+        return false;
+    }
+
+    public void open(int i, int j) {
+
+    }
+}
